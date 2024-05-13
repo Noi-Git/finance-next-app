277a9350acdf980d46e3d3fd8f8b034a87c25e93
Noi-Git <noi.patsin@gmail.com>
2024-05-13 14:11:01 -0400

    fix missing pages and test all pages routes

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { homeContainer } from '@/components/styles/home-style'
+
+export default function Home() {
+  return (
+    <>
+      <main className={homeContainer}>This is going to a home page</main>
+    </>
+  )
+}
