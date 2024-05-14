9bb1f59c13e52ba3daebaab8ea84ef65e9b2e3db
Noi-Git <noi.patsin@gmail.com>
2024-05-14 16:05:08 -0400

    add more styles

--- src/components/styles/table-style.ts
+++ src/components/styles/table-style.ts
@@ -6,7 +6,7 @@ export const tableWrapper = ' text-sky-700'
 export const tableBody = 'flex flex-col justify-between  h=[calc(100vh - 3rem)]'
 
 export const tableHead =
-  'flex flex-row justify-between text-center w-[calc(90vw)] font-bold py-5 bg-sky-100 border-b-2 border-b-sky-600'
+  'flex flex-row justify-between text-center w-[calc(90vw)] font-bold py-5 bg-teal-300 border-b-2 border-b-sky-600'
 
 export const thName = 'w-[calc(300px)]'
 export const thAmount = 'w-[calc(150px)]'
@@ -14,7 +14,7 @@ export const thDate = 'w-[calc(100px)]'
 export const thType = 'w-[calc(250px)]'
 
 export const tableRow =
-  'flex flex-row justify-between text-center w-[calc(90vw)] py-1.5 gap-4 odd:bg-sky-50'
+  'flex flex-row justify-between text-center w-[calc(90vw)] py-1.5 gap-4 odd:bg-teal-100'
 
 export const tdName = 'w-[calc(300px)]'
 export const tdAmount = 'w-[calc(150px)]'
