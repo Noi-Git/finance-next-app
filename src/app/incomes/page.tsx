f05d1341ce18200ef4bd77bb2728a74e37513e65
Noi-Git <noi.patsin@gmail.com>
2024-05-14 20:40:40 -0400

    add condition to show component on income dashboard

--- src/app/incomes/page.tsx
+++ src/app/incomes/page.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { fetchIncome } from '@/utils/incomeHelper'
 import IncomeType from './income-type/page'
 import IncomeItem from './income-item/page'
 import IncomeDetails from './income-details/page'
 import IncomeTable from './income-table/page'
 
 const Incomes = () => {
+  const incomeBudget = false
+
   return (
     <>
-      <IncomeType />
-      <IncomeItem />
-      <IncomeDetails />
-      <IncomeTable />
+      {/* if !user.income ? <IncomeType /> <IncomeItem /> :  <IncomeType /> <IncomeItem /> <IncomeDetails /> <IncomeTable />*/}
+      {!incomeBudget ? (
+        <>
+          <IncomeType />
+          <IncomeItem />
+        </>
+      ) : (
+        <>
+          <IncomeType />
+          <IncomeItem />
+          <IncomeDetails />
+          <IncomeTable />
+        </>
+      )}
     </>
   )
 }
