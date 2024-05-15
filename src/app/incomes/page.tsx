82a45585b047658cdc196db92909d0c34e7306d5
Noi-Git <noi.patsin@gmail.com>
2024-05-15 18:35:01 -0400

    modify income dashboard file without condition showing

--- src/app/incomes/page.tsx
+++ src/app/incomes/page.tsx
@@ -1,28 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IncomeType from './income-type/page'
 import IncomeItem from './income-item/page'
 import IncomeDetails from './income-details/page'
 import IncomeTable from './income-table/page'
 
 const Incomes = () => {
-  const incomeBudget = false
-
   return (
     <>
-      {/* if !user.income ? <IncomeType /> <IncomeItem /> :  <IncomeType /> <IncomeItem /> <IncomeDetails /> <IncomeTable />*/}
-      {!incomeBudget ? (
-        <>
-          <IncomeType />
-          <IncomeItem />
-        </>
-      ) : (
-        <>
-          <IncomeType />
-          <IncomeItem />
-          <IncomeDetails />
-          <IncomeTable />
-        </>
-      )}
+      <IncomeType />
+      <IncomeItem />
+      <IncomeDetails />
+      <IncomeTable />
     </>
   )
 }
