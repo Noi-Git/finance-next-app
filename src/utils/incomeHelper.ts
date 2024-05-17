877e911d6de427ebb329fb3b7d2a2aa2083efc32
Noi-Git <noi.patsin@gmail.com>
2024-05-17 12:42:01 -0400

    create helper function to add budget to database

--- src/utils/incomeHelper.ts
+++ src/utils/incomeHelper.ts
@@ -1,16 +1,16 @@
+import { UseCreateIncome, UseIncomeBudget } from '@/app/incomes/page'
+
 export const formatDateToLocalString = (epoch: any) =>
   new Date(epoch).toLocaleDateString()
 
 export const createIncomeBudget = ({ ib_name, ib_amount }: any) => {
+  const { data, error, isLoading } = UseIncomeBudget()
   const newItem = {
     ib_name,
     ib_amount: +ib_amount,
   }
-  // const existingBudgets = getData() ?? []
-  // return localStorage.setItem(
-  //   'budgets',
-  //   JSON.stringify([...existingBudgets, newItem])
-  // )
+  const existingBudgets = data ?? []
+  return UseCreateIncome(), JSON.stringify([...existingBudgets, newItem])
 }
 
 export const createIncomeItem = ({ ib_name, ib_amount }: any) => {
