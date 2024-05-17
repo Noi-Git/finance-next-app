6ced4656e81cfe2961cd6226fc7cf9de14cfcef4
Noi-Git <noi.patsin@gmail.com>
2024-05-17 00:37:33 -0400

    create reusable income useQuery

--- src/app/incomes/page.tsx
+++ src/app/incomes/page.tsx
@@ -3,6 +3,16 @@ import IncomeType from './income-type/page'
 import IncomeItem from './income-item/page'
 import IncomeDetails from './income-details/page'
 import IncomeTable from './income-table/page'
+import { useSession } from 'next-auth/react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+export function useIncomeBudget() {
+  return useQuery({
+    queryKey: ['incomeBudget'],
+    queryFn: () =>
+      fetch('http://localhost:3000/api/itype').then((res) => res.json()),
+  })
+}
 
 const Incomes = () => {
   return (
