e7899caf8407cb58ae015bd80bfb5c5e9e3b7367
Noi-Git <noi.patsin@gmail.com>
2024-05-17 15:12:43 -0400

    client fetch

--- src/app/api/iitem/route.ts
+++ src/app/api/iitem/route.ts
@@ -1,16 +1,107 @@
+import { getAuthSession } from '@/utils/auth'
 import { prisma } from '@/utils/connect'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+// export const GET = async () => {
+//   try {
+//     const iitem = await prisma.incomeItem.findMany()
+//     return new NextResponse(JSON.stringify(iitem), { status: 200 })
+//   } catch (err) {
+//     console.log('fetch income type error: --', err)
+//     return new NextResponse(
+//       JSON.stringify({ message: 'Something went wrong!' }),
+//       { status: 500 }
+//     )
+//   }
+// }
 
 export const GET = async () => {
   try {
     const iitem = await prisma.incomeItem.findMany()
     return new NextResponse(JSON.stringify(iitem), { status: 200 })
   } catch (err) {
-    console.log('fetch income type error: --', err)
+    console.log('fetch income item error: --', err)
     return new NextResponse(
       JSON.stringify({ message: 'Something went wrong!' }),
       { status: 500 }
     )
+  }
+}
+
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession()
+  if (session) {
+    try {
+      const body = await req.json()
+
+      const incomeItem = await prisma.incomeItem.create({
+        data: body,
+      })
+      console.log(
+        'sucessful from post backend endpoint - add new item to database'
+      )
+      return new NextResponse(JSON.stringify(body), { status: 201 })
+    } catch (err) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      )
+    }
+  }
+}
+
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params
+  const session = await getAuthSession()
+  if (session) {
+    try {
+      const body = await req.json()
+
+      await prisma.incomeItem.update({
+        where: {
+          i_id: id,
+        },
+        data: body,
+      })
+      return new NextResponse(JSON.stringify({ message: 'Updated' }), {
+        status: 200,
+      })
+    } catch (err) {
+      console.log(err)
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      )
+    }
+  }
+}
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params
+  const session = await getAuthSession()
+  if (session) {
+    try {
+      await prisma.incomeItem.delete({
+        where: {
+          i_id: id,
+        },
+      })
+      return new NextResponse(JSON.stringify({ message: 'Deleted' }), {
+        status: 200,
+      })
+    } catch (err) {
+      console.log(err)
+      return new NextResponse(
+        JSON.stringify({ message: 'Something went wrong' }),
+        { status: 500 }
+      )
+    }
   }
 }
 
