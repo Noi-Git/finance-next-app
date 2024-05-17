4b44aa333f6409d524368422132df9074e9547f4
Noi-Git <noi.patsin@gmail.com>
2024-05-17 01:22:07 -0400

    create useMutation Hook

--- src/app/incomes/page.tsx
+++ src/app/incomes/page.tsx
@@ -5,12 +5,29 @@ import IncomeDetails from './income-details/page'
 import IncomeTable from './income-table/page'
 import { useSession } from 'next-auth/react'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { IBudget } from '@/types/types'
 
 export function useIncomeBudget() {
   return useQuery({
     queryKey: ['incomeBudget'],
     queryFn: () =>
       fetch('http://localhost:3000/api/itype').then((res) => res.json()),
+  })
+}
+// export const QueryClient = useQueryClient()
+export function useCreateIncome() {
+  const QueryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (newBudget) => {
+      return fetch('http://http://localhost:3000/api/itype', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newBudget),
+      })
+    },
+    onSuccess() {
+      QueryClient.invalidateQueries({ queryKey: ['incomeBudget'] })
+    },
   })
 }
 
